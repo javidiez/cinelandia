48ec4068222630692a09900e078984b40bdeb3c6
https://github.com/javidiez/ <javierdiez92@gmail.com>
2024-06-21 18:10:44 +0200

    fix net 4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Ajusta la ruta base según sea necesario
+  base: '/', // Ajusta la ruta base si es necesario
   plugins: [
     react({
       fastRefresh: true, // Habilitar Fast Refresh
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html', // Asegúrate de que este es el archivo correcto
+      }
+    }
+  }
 });
