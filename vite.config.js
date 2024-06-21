9019db031d5e916049ae639395d9330c126dbd32
https://github.com/javidiez/ <javierdiez92@gmail.com>
2024-06-21 16:10:32 +0200

    fix navbar

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Ajusta la ruta base según sea necesario
   plugins: [
     react({
       fastRefresh: true, // Habilitar Fast Refresh
