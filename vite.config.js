5865f822108bd41c85db8155b2487120a5c96c9e
https://github.com/javidiez/ <javierdiez92@gmail.com>
2024-06-22 16:23:39 +0200

    UPD main name

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Ajusta la ruta base según sea necesario
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   build: {
-    rollupOptions: {
-      input: './src/index.jsx', // Ruta al archivo JavaScript o TypeScript principal
-    },
-    outDir: 'dist', // Reemplaza con tu directorio de compilación si es diferente
-  }
+    outDir: 'dist', // Directorio de salida para los archivos construidos
+    emptyOutDir: true, // Limpiar directorio de salida antes de construir
+  },
 });
