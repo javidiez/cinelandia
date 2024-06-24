11a1b386087118439cb53b9ca9d22ce817da553f
https://github.com/javidiez/ <javierdiez92@gmail.com>
2024-06-24 16:14:14 +0200

    Descomentar en index

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom'],
+  },
   build: {
     rollupOptions: {
       input: {
