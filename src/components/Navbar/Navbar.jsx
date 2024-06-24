2976bb0d8607ce1bfbb2a5494ecffd746fdf3189
https://github.com/javidiez/ <javierdiez92@gmail.com>
2024-06-24 16:25:06 +0200

    Descomentar en index

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -37,7 +37,7 @@ export const Navbar = () => {
             <div className="container-fluid">
                 <div className="d-flex justify-content-between">
                     <Link className="navbar-brand d-flex align-items-center" to="/home">
-                        {/* <img src={logo} className="logo" /> */}
+                        <img src={logo} className="logo" />
                         <p className="text-light fw-bold ps-3">CINELANDIA</p>
                     </Link>
                 </div>
